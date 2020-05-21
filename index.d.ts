18f70a7e74a78604ad8d1acda07868ce4bfb1977
Leandro Simões <leandro.simoes@outlook.com>
2020-05-21 19:51:36 -0300

    Fix icon typings

--- index.d.ts
+++ index.d.ts
@@ -2,6 +2,11 @@ declare module "react-native-fontawesome" {
   import React from "react";
   import { TextProperties } from "react-native";
   export { SolidIcons, RegularIcons, BrandIcons, parseIconFromClassName } from "react-native-fontawesome/FontAwesomeIcons";
-  const Icon: React.ComponentClass<TextProperties>;
+
+  interface IIconProps extends TextProperties {
+    icon: string
+  }
+
+  const Icon: React.ComponentClass<IIconProps>;
   export default Icon;
 }
